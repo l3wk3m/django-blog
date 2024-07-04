2b27a509c645f38176c43cfb618a309bda29c99a
Luke Maycock <maycock.luke@gmail.com>
2024-07-04 13:12:33 +0000

    Worked through some troubleshooting steps with CI tutor support

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("key1234")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("postgres://uxo3hooruft:eI0IJJmFqYQQ@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/rack_shady_quake_165897"))
+     'default': dj_database_url.parse(os.environ.get("DATABASE_URL")) 
 }
 
 # Password validation
